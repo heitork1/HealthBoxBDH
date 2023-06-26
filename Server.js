9f93d2b92ed66cd00f0cb1e6220c98fcff01a7cd
heitork1 <heitorklein70@gmail.com>
2023-06-25 21:44:09 -0300

    puxando dados em minha-conta

--- Server.js
+++ Server.js
@@ -11,11 +11,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
 
-const path = require('path')
+
+
+const path = require('path');
+const { error } = require('console');
 app.use('/assets', express.static('assets'))
 app.use('/img', express.static('img'))
 app.use('/pages', express.static('pages'))
 app.use(expressLayouts)
+app.set('views', path.join(__dirname, 'views'));
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -38,28 +42,46 @@ connection.connect(function (err) {
   }
 });
 
+function insertUsernameIntoHTML(filePath, username) {
+  let html = fs.readFileSync(filePath, 'utf8');
+  html = html.replace('<p id="nomeUsuario">Entrar</p>', `<p id="nomeUsuario">${username}</p>`);
+  return html;
+}
+
+function checkLogin(req, res, next) {
+  if (req.session.username) {
+    next(); // Continua para a próxima rota/middleware
+  } else {
+    res.redirect('/pages/login.html'); // Redireciona para a página de login
+  }
+}
 app.get('/', (req, res) => {
   if (req.session.username) {
     // Usuário está logado
-    const filePath = path.join(__dirname, 'index.html');
-    const html = insertUsernameIntoHTML(filePath, req.session.username);
-    res.send(html);
-  } else {
-    // Usuário não está logado
-    res.sendFile(__dirname + '/index.html');
-  }
-});
-
-app.get('/pages/produtos', (req, res) => {
-  if (req.session.username) {
-    const filePath = path.join(__dirname, 'pages', 'produtos.ejs');
+    const filePath = path.join(__dirname, 'views', 'index.ejs');
     const html = fs.readFileSync(filePath, 'utf8');
     const renderedHtml = ejs.render(html, { nomeUsuario: req.session.username });
     res.send(renderedHtml);
   } else {
-    res.redirect('/pages/login.html');
+    // Usuário não está logado
+    res.render('index');
   }
 });
+
+app.get('/pages/produtos', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'produtos.ejs');
+  const html = fs.readFileSync(filePath, 'utf8');
+  const renderedHtml = ejs.render(html, { nomeUsuario: req.session.username });
+  res.send(renderedHtml);
+});
+
+app.get('/pages/sobre-nos', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'sobre-nos.ejs');
+  const html = fs.readFileSync(filePath, 'utf8');
+  const renderedHtml = ejs.render(html, { nomeUsuario: req.session.username });
+  res.send(renderedHtml);
+});
+
 
 
 app.post('/login', (req, res) => {
@@ -74,7 +96,7 @@ app.post('/login', (req, res) => {
         if (password === senhaBanco) {
           console.log('Senha correta! Acesso permitido.');
           req.session.username = rows[0].nome;
-
+          req.session.userID = rows[0].id
           res.send(rows[0].nome);
         } else {
           res.send('Senha incorreta');
@@ -93,6 +115,40 @@ app.post('/login', (req, res) => {
 app.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/pages/login.html');
+});
+
+app.get('/pages/minha-conta', checkLogin, (req, res) => {
+  
+
+  connection.query("SELECT * FROM usuarios WHERE id = '" + req.session.userID + "'", function (err, rows) {
+    if (!err) {
+      req.session.email = rows[0].email;
+      req.session.date = rows[0].data_nascimento
+      req.session.senha = rows[0].senha
+      req.session.tel = rows[0].telefone
+      console.log(req.session.date)
+      const filePath = path.join(__dirname, 'pages', 'minha-conta.ejs');
+      const html = fs.readFileSync(filePath, 'utf8');
+      const renderedHtml = ejs.render(html, 
+        { nomeUsuario: req.session.username,
+          emailUsuario: req.session.email,
+          nascimentoUsuario: req.session.date,
+          senhaUsuario: req.session.senha,
+          telefoneUsuario: req.session.tel
+        });
+      res.send(renderedHtml);
+    } else {
+      res.send(err)
+    }
+  })
+
+});
+
+app.get('/pages/sacola', checkLogin, (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'sacola.ejs');
+  const html = fs.readFileSync(filePath, 'utf8');
+  const renderedHtml = ejs.render(html, { nomeUsuario: req.session.username});
+  res.send(renderedHtml);
 });
 
 app.post('/cadastro', (req, res) => {
@@ -114,22 +170,9 @@ connection.query("INSERT INTO usuarios (`nome`, `email`, `senha`,`telefone`, `da
 })
 
 
-app.get('/pages/minha-conta.html', (req, res) => {
-  if (req.session.username) {
-    const filePath = path.join(__dirname, 'pages', 'minha-conta.html');
-    const html = fs.readFileSync(filePath, 'utf8');
-    const renderedHtml = ejs.render(html, { nomeUsuario: req.session.username });
-    res.send(renderedHtml);
-  } else {
-    res.redirect('/pages/login.html');
-  }
-});
 
-function insertUsernameIntoHTML(filePath, username) {
-  let html = fs.readFileSync(filePath, 'utf8');
-  html = html.replace('<p id="nomeUsuario">Entrar</p>', `<p id="nomeUsuario">${username}</p>`);
-  return html;
-}
+
+
 
 app.listen(3002, () => {
   console.log('Servidor rodando na porta 3002!')
